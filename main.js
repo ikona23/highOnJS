e866cd8de44c2b4ed6f45e51a359bc78780e3165
ikona23 <ikona23@gmail.com>
2017-10-15 19:11:56 +1300

    add name, choose lesson and heaps of animation

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+//console.log(document.head)
+//console.log(document.forms[0])
+//console.log(document.links)
+//console.log(document.getElementById())
+
+//document.getElementById('texted')
+
+
+
 var playPause = anime ({
 	targets: 'div.box',
 	translateY: [
@@ -12,6 +21,7 @@ var playPause = anime ({
 		return i * 1000},
 		autoplay:false
 })
+
 
 // var textTrip = anime ({
 // 	targets: 'div.text',
@@ -28,14 +38,33 @@ var playPause = anime ({
 // 		autoplay:false
 // })
 
+function getNames(){
+	var n = document.getElementById("icon_prefix").value
+	var s = document.getElementById("icon_prefix2").value
+    var m = document.getElementById("icon_prefix4").textContent
+	var t = document.getElementById('texted')
+    t.textContent = 'Hi' + " " + n + " " + s + " " + "you choose" + " " + m
+}
+
+function addBox(){
+	var x = document.getElementById('newText')
+//	console.log(x)
+}
+addBox()
+
+
+
+
 function dropTest(){
 	var cssSel=anime({
 		targets: '.textTrip',
 		translateY: -220,
   		backgroundColor: '#FFF',
-  		borderRadius: 40,
+  		borderRadius: 5,
+  		width:350,
+
   		easing: 'easeInOutQuad',
-  		  backgroundColor: [
+  		backgroundColor: [
     {value: '#FFF'}, // Or #FFFFFF
     {value: 'rgb(255, 0, 0)'},
     {value: 'hsl(100, 60%, 60%)'}
